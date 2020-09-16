72138659686841713b0f90d1347d5a6cef4e267a
Khoirul Fahmi <70826724+kfahmi77@users.noreply.github.com>
2020-09-16 09:57:16 +0700

    Add files via upload
    Tugas Jurnal 1 Implementasi Algoritma

---
+++ Coba.java
@@ -0,0 +1,7 @@
+public class Coba{
+public static void main(String[]args){
+	System.out.println("Nama : Khoirul Fahmi");
+	System.out.println("NIM  : 6706204026");
+	System.out.println("Kelas: d3-4402");
+}
+}
