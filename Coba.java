f82f07c6a014359f2ce63b845f3435c83d76ff72
Khoirul Fahmi <70826724+kfahmi77@users.noreply.github.com>
2020-09-19 10:55:10 +0700

    Delete Coba.java

--- Coba.java
+++
@@ -1,7 +0,0 @@
-public class Coba{
-public static void main(String[]args){
-	System.out.println("Nama : Khoirul Fahmi");
-	System.out.println("NIM  : 6706204026");
-	System.out.println("Kelas: d3-4402");
-}
-}
